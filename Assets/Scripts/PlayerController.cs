ec3eb46e30bf7512473f148f378a27c42040c641
Steve_U57 <obsidian77@gmail.com>
2021-04-08 19:38:38 -0400

    82_Set_Up_Firing_Input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -22,6 +23,19 @@ public class PlayerController : MonoBehaviour
     {
         ProcessTranslation();
         ProcessRotation();
+        ProcessFiring();
+    }
+
+    void ProcessFiring()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Debug.Log("Pew Pew!");
+        }
+        else
+        {
+            Debug.Log("Sup");
+        }
     }
 
     void ProcessRotation()
